2105846ef231697ee4b5e2780befa4c083e335b1
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2013-09-20 10:40:11 +0300

    Added ssh-copy-id.sh script.

---
+++ scripts/ssh-copy-id.sh
@@ -0,0 +1,19 @@
+#!/usr/sh
+
+check_args()
+{
+    if [ "$#" -eq 0 ] ; then
+        echo Usage $0 user@machine
+        exit 0
+    fi
+}
+
+main()
+{
+    [ ! -f ~/.ssh/id_rsa.pub ] && ssh-keygen -t rsa
+    cat ~/.ssh/id_rsa.pub | ssh $1 \
+        "mkdir -p ~/.ssh; cat >> ~/.ssh/authorized_keys"
+}
+
+check_args $*
+main $1
